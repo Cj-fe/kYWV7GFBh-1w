d5e04756abc6cb4b52697dda498e45ae1b9469ea
Cj-fe <us1071591@gmail.com>
2024-12-04 14:07:32 +0800

    commit

--- generate_hash.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Calculate the initial hash of index.php
-$originalHash = hash_file('sha256', 'index.php');
-
-// Store the hash in a file
-file_put_contents('index_hash.txt', $originalHash);
-
-echo "Original hash calculated and stored.";
-?>
