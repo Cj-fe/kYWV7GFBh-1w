b9c4b86f0d3e6f0e8858fcdc3fc75eb54964b91b
Cj-fe <us1071591@gmail.com>
2024-12-15 17:42:33 +0800

    commit

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ $data = array_slice($data, 0, 5);
         }
 
         /* Add this CSS to your existing styles */
-        @media screen (max-width: 768px) {
+        @media screen (max-width: 408px) {
             .hide-on-mobile {
                 display: none;
             }
