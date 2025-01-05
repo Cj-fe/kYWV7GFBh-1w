4e1cb114332c25ba09235ef8bb118e14a391df7e
Cj-fe <us1071591@gmail.com>
2025-01-05 14:08:39 +0800

    commit

--- userpage/update_account.php
+++ userpage/update_account.php
@@ -976,6 +976,9 @@
             document.getElementById('monthly_income'),
             document.getElementById('date_for_current_employment'),
             document.getElementById('first_employment_date'),
+            document.getElementById('work_classification'),
+            document.getElementById('related_to_course'),
+            document.getElementById('job_satisfaction'),
             
             // Add other fields here
         ];
