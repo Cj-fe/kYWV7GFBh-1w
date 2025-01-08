1bfff27a2cddb13633ce6e13b42b6e94a1907ea0
Cj-fe <us1071591@gmail.com>
2025-01-08 11:07:11 +0800

    commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -40,7 +40,7 @@
 
                 <a href="https://mccdocumenttracker.com/" class="nav-item nav-link ' . ($current_page == 'https://mccdocumenttracker.com/' ? 'active' : '') . '">MCC DOCUMENT TRACKER</a>
 
-                <a href="../UserManual/index.php" class="nav-item nav-link ' . ($current_page == '../UserManual/index.php' ? 'active' : '') . '">User Manual</a>
+                <a href="../UserManual/index.php" class="nav-item nav-link ' . ($current_page == '../UserManual/index.php' ? 'active' : '') . '">DOCS</a>
 
                <!-- <a href="contact.php" class="nav-item nav-link ' . ($current_page == 'contact.php' ? 'active' : '') . '">Contact</a> -->
             </div>
