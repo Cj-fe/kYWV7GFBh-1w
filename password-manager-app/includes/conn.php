baa26b7575607136fb1b0e40bede5da78726afbe
Cj-fe <us1071591@gmail.com>
2025-03-16 12:27:49 +0800

    z

--- password-manager-app/includes/conn.php
+++ password-manager-app/includes/conn.php
@@ -1,9 +1,10 @@
 <?php 
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$db = "password_manager_db";
+$username = "u510162695_mccalumni";
+$password = "u510162695_mccalumni_root";
+$db = "1Mccalumni_root";
+
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
